8faee1fdef27e795b203d682f0486b563456c371
fritzSchool <lo2257tq@go.minnstate.edu>
2022-03-10 08:01:06 -0600

    Pulled in main, colinbolduc/vendors and Rich/changedAgent and resolved conflicts

--- Group7Proj/src/main/java/com/group7/View/HomepageView.java
+++ Group7Proj/src/main/java/com/group7/View/HomepageView.java
@@ -1,5 +1,10 @@
 package com.group7.View;
 
+import com.group7.controllers.AgentManageController;
+import com.group7.model.AgentModel;
+import com.group7.model.LoginModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -7,17 +12,14 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -35,11 +37,6 @@ public class HomepageView implements Initializable {
         stage.close();
     }
 
-    public void logOutButtonOnAction(ActionEvent event) throws IOException {
-        loginScreen();
-        Stage stage = (Stage) exitButton.getScene().getWindow();
-        stage.close();
-    }
 
     public void agentMainButtonOnAction(ActionEvent event) throws IOException {
         if(LoginModel.admin) {
@@ -52,30 +49,6 @@ public class HomepageView implements Initializable {
             Stage stage = (Stage) exitButton.getScene().getWindow();
             stage.close();
         }
-    }
-
-    public void ownerMainButtonOnAction(ActionEvent event) throws IOException {
-        ownerMainScreen();
-        Stage stage = (Stage) exitButton.getScene().getWindow();
-        stage.close();
-    }
-
-    public void propertyMainButtonOnAction(ActionEvent event) throws IOException {
-        propertyMainScreen();
-        Stage stage = (Stage) exitButton.getScene().getWindow();
-        stage.close();
-    }
-
-    public void guestMainButtonOnAction(ActionEvent event) throws IOException {
-        guestMainScreen();
-        Stage stage = (Stage) exitButton.getScene().getWindow();
-        stage.close();
-    }
-
-    public void vendorMainButtonOnAction(ActionEvent event) throws IOException {
-        vendorMainScreen();
-        Stage stage = (Stage) exitButton.getScene().getWindow();
-        stage.close();
     }
 
     public void logOutButtonOnAction() {
@@ -94,7 +67,7 @@ public class HomepageView implements Initializable {
         stage.close();
     }
 
-    public void agentMainButtonOnAction() {
+    public void agentMainScreen() {
         try{
             Parent root = FXMLLoader.load(getClass().getResource("/com/group7/Agent/agentMain.fxml"));
             Stage registerStage = new Stage();
@@ -108,6 +81,8 @@ public class HomepageView implements Initializable {
         Stage stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
     }
+
+
 
     public void ownerMainButtonOnAction() {
         try{
